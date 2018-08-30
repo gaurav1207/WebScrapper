8d37bd81789c9d11ed809a2a3c9398a1421f7eab
gaurav1207 <32225208+gaurav1207@users.noreply.github.com>
2018-08-30 15:32:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ h=requests.get(b)
 s=bs4.BeautifulSoup(h.text,'lxml')
 k=[]
 o=0;
-file=open("test.txt","w")
+file=open("Text file name to safe the info at","w")
 for i in s.find_all('a',href=True):
   
   if(i['href'][0]=='h'):
